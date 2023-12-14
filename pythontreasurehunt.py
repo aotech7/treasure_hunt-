1c5734adb47272e4dbfc259f8cefd9a1728f71fc
AOTech <86697379+aotech7@users.noreply.github.com>
2023-12-13 21:23:33 -0800

    Add files via upload

---
+++ pythontreasurehunt.py
@@ -0,0 +1,32 @@
+# python treasure hunt game
+import random
+import time
+
+
+def display_game_intro():
+    print('''Here is a 5 sentence introduction to a Python treasure hunt game:
+
+Search for Hidden Treasures: A Python Adventure
+
+Eager for adventure and riches beyond your wildest dreams? Embark on and epic quest in search of hidden treasures and lost artifacts using your trusty Python skills. Solving puzzles and overcoming challenges at each stage, you'll traverse mysterious ruins, secret passages, and ancient temples on your way towards the ultimate prize. With danger, surprises, and fun around every corner, this Python-powered treasure hunt promises an experience you won't soon forget. Master loops, strings, lists, and more as you learn to think like a true adventurer-archeologist. The treasures await - do you have the coding chops to uncover their secrets?''')
+
+#functions should be self describing and don't run until they are called. 
+    
+def choose_cave():
+    pass
+
+def enter_cave():
+    pass
+
+
+def main_loop():
+    display_game_intro()
+
+
+def main_loop():
+    display_game_intro()
+
+#function is in statis until you call it
+
+main_loop()
+                                                         
