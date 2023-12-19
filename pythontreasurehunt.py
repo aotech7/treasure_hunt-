ea10875eead596794952986f3fa175d77a03f36e
AOTech <86697379+aotech7@users.noreply.github.com>
2023-12-18 23:38:45 -0800

    Add files via upload

--- pythontreasurehunt.py
+++ pythontreasurehunt.py
@@ -13,20 +13,66 @@ Eager for adventure and riches beyond your wildest dreams? Embark on and epic qu
 #functions should be self describing and don't run until they are called. 
     
 def choose_cave():
-    pass
+    cave  = ''
+    while cave != '1' and cave != '2':
+        print('Which cave do you want to enter? (1 or 2)')
+        cave = input()
+    return cave 
 
-def enter_cave():
-    pass
+def enter_cave(chosen_cave):
+
+    print("You have entered a cave...")
+#stops the program for one second
+    time.sleep(1)
 
 
+    #asking for a random number between 1 and 2
+    random_cave = random.randint(1,2)
+
+    #print(random_cave = + str(random+cave))
+
+    if random_cave == int(chosen_cave):
+          print("----> Lucky you! You found the cave!")
+    else:
+        print("----> Aye, mate all  you get is booty filled with...\n Week old socks!")
 def main_loop():
     display_game_intro()
 
 
 def main_loop():
     display_game_intro()
+    whatdidenter = choose_cave()
+
+    print("What cave did I choose: " + whatdidenter)
 
 #function is in statis until you call it
+#choose cave returns whatever value is in cave
+
+def main_loop():
+    
+    '''the main loop function controls the flow of the game by calling functions and using conditionals'''
+
+    playGameAgain = "yes"
+
+    while playGameAgain == "yes" or playGameAgain == "y":
+
+        display_game_intro()
+        chosen_cave = choose_cave()
+        enter_cave(chosen_cave)
+
+        print("\n\nDo you want to try again? (yes or no)")
+        playGameAgain = input()
+        print("You said" + ' '+ playGameAgain)
+        time.sleep(1)
+
+        if playGameAgain == "yes" or playGameAgain == "y":
+            print("\nLet'stryagain")
+
+        else:
+            print("Ok, hasta luego!")
+
 
 main_loop()
-                                                         
+
+
+                     
